42053cfad8f4d5a1b458df10a73a7d43e89b963f
MuhammadFaiz54 <70813541+MuhammadFaiz54@users.noreply.github.com>
2021-01-20 21:49:24 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+var pehela = "Faiz sab se pehele aya han class lene thek 6:48 pe"
+
+// console.log("pehela------", pehela)
+
+for(var i = pehela.length -1; i > 0 ; i--) {
+ console.log(pehela[i])
+}
